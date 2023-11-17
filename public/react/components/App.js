66f916e9b2067280fad105a1e8e67b66530e4b21
PotticusMaximus <136069407+PotticusMaximus@users.noreply.github.com>
2023-11-17 12:17:44 +0000

    step one done - list populates, articles can be read and exited

--- public/react/components/App.js
+++ public/react/components/App.js
@@ -1,32 +1,44 @@
-import React, { useState, useEffect } from 'react';
-import { PagesList } from './PagesList';
+import React, { useState, useEffect } from "react";
+import { PagesList } from "./PagesList";
 
 // import and prepend the api url to any fetch calls
-import apiURL from '../api';
+import apiURL from "../api";
 
 export const App = () => {
+  const [pages, setPages] = useState([]);
+  const [article, setArticle] = useState("");
 
-	const [pages, setPages] = useState([]);
+  async function fetchPages() {
+    try {
+      const response = await fetch(`${apiURL}/wiki`);
+      const pagesData = await response.json();
+      setPages(pagesData);
+    } catch (err) {
+      console.log("Oh no an error! ", err);
+    }
+  }
 
-	async function fetchPages(){
-		try {
-			const response = await fetch(`${apiURL}/wiki`);
-			const pagesData = await response.json();
-			setPages(pagesData);
-		} catch (err) {
-			console.log("Oh no an error! ", err)
-		}
-	}
+  useEffect(() => {
+    fetchPages();
+  }, []);
 
-	useEffect(() => {
-		fetchPages();
-	}, []);
+  const getArticle = async (slug) => {
+    const response = await fetch(`${apiURL}/wiki/${slug}`);
+    const data = await response.json();
+    setArticle(data.content);
+  };
 
-	return (
-		<main>	
-      <h1>WikiVerse</h1>
-			<h2>An interesting 📚</h2>
-			<PagesList pages={pages} />
-		</main>
-	)
-}
+  return (
+    <main>
+      <h1>Brickipedia</h1>
+      <h2>Solid Reading</h2>
+      {article === "" && <PagesList pages={pages} getArticle={getArticle} />}
+      {article !== "" && (
+        <>
+          <p>{article}</p>
+          <button onClick={() => setArticle("")}>Back to Brickpedia</button>
+        </>
+      )}
+    </main>
+  );
+};
--- public/react/components/Page.js
+++ public/react/components/Page.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React from "react";
 
 export const Page = (props) => {
-
-  return <>
-    <h3>{props.page.title}</h3>
-  </>
-} 
-	
+  return (
+    <>
+      <h3>{props.page.title}</h3>
+      <button onClick={() => props.getArticle(props.page.slug)}>Read</button>
+    </>
+  );
+};
+//on click function will reference props.page.slug
