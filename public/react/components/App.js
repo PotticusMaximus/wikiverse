4128dfe2ee2f3907fc993b602fc204d19892f705
PotticusMaximus <136069407+PotticusMaximus@users.noreply.github.com>
2023-11-17 13:02:34 +0000

    create page part done

--- public/react/components/App.js
+++ public/react/components/App.js
@@ -7,6 +7,11 @@ import apiURL from "../api";
 export const App = () => {
   const [pages, setPages] = useState([]);
   const [article, setArticle] = useState("");
+  const [addingArticle, setAddingArticle] = useState(false);
+
+  const [formData, setFormData] = useState([
+    { title: "", content: "", name: "", email: "", tags: "" },
+  ]);
 
   async function fetchPages() {
     try {
@@ -28,15 +33,103 @@ export const App = () => {
     setArticle(data.content);
   };
 
+  function handleChange(e) {
+    e.preventDefault;
+    setFormData(e.target.value);
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault;
+    const response = await fetch(`${apiURL}/wiki/`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        title: formData.title,
+        content: formData.content,
+        name: formData.name,
+        email: formData.email,
+        tags: formData.tags,
+      }),
+    });
+    const data = await response.json();
+    //finish setting form data and resetting etc
+  }
+
   return (
     <main>
       <h1>Brickipedia</h1>
       <h2>Solid Reading</h2>
-      {article === "" && <PagesList pages={pages} getArticle={getArticle} />}
-      {article !== "" && (
+      {article === "" && addingArticle === false && (
+        <>
+          <PagesList pages={pages} getArticle={getArticle} />
+          <div>
+            <button onClick={() => setAddingArticle(true)}>Create</button>
+          </div>
+        </>
+      )}
+      {article !== "" && addingArticle === false && (
         <>
           <p>{article}</p>
           <button onClick={() => setArticle("")}>Back to Brickpedia</button>
+        </>
+      )}
+      {addingArticle === true && (
+        <>
+          <form //</>onSubmit={handleSubmit}
+          >
+            <label>
+              Title:
+              <input
+                type="text"
+                name="title"
+                value={formData.title}
+                onChange={handleChange}
+              />
+            </label>
+            <br />
+            <label>
+              Content:
+              <textarea
+                name="content"
+                value={formData.content}
+                onChange={handleChange}
+              />
+            </label>
+            <br />
+            <label>
+              Author Name:
+              <input
+                type="text"
+                name="authorName"
+                value={formData.name}
+                onChange={handleChange}
+              />
+            </label>
+            <br />
+            <label>
+              Author Email:
+              <input
+                type="email"
+                name="authorEmail"
+                value={formData.email}
+                onChange={handleChange}
+              />
+            </label>
+            <br />
+            <label>
+              Tags:
+              <input
+                type="text"
+                name="tags"
+                value={formData.tags}
+                onChange={handleChange}
+              />
+            </label>
+            <br />
+            <button type="submit">Submit</button>
+          </form>
         </>
       )}
     </main>
